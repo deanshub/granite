1ce7c70a684081e1ee111bf312c529eac3074e36
Dean Shub <deanshub@gmail.com>
2025-12-01 14:31:04 +0200

    fix issue

--- app/simple/page.tsx
+++
@@ -1,6 +0,0 @@
-import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
-
-export default function Page() {
-  // TODO: put something else here
-  return <SimpleEditor content="" onChange={() => {}} />
-}
